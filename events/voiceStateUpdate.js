d6c1a80f1d025f971551d06037707904eca53be5
moccacola <158811185+moccacola@users.noreply.github.com>
2024-02-04 21:54:15 +0700

    voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -10,7 +10,7 @@ if(botChannel){
 if(botChannel.id == oldState.channelId)
 if(botChannel?.members?.find(x => x == client?.user?.id)){
 if(botChannel?.members?.size == 1){
-await queue?.textChannel?.send({ content: `🔴 Users left channel!!` }).catch(e => { })
+await queue?.textChannel?.send({ content: `ogt mainya tinggal"an!!` }).catch(e => { })
 if(queue || queue?.playing){
 return queue?.stop(oldState.guild.id)
 }
@@ -30,7 +30,7 @@ await queue?.pause()
 } catch(e){
 return 
 }
-await queue?.textChannel?.send({ content: `🔴 Muted` }).catch(e => { })
+await queue?.textChannel?.send({ content: `🔴 Terbungkam` }).catch(e => { })
 }
 }
 if(oldState.serverMute === true && newState.serverMute === false){
