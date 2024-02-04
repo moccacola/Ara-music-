c0708cf92b6815993c96563e1f134dddcf6dd39d
moccacola <158811185+moccacola@users.noreply.github.com>
2024-02-04 22:48:37 +0700

    loop.js

--- commands/loop.js
+++ commands/loop.js
@@ -16,7 +16,7 @@
 const db = require("../mongoDB");
 module.exports = {
   name: "loop",
-  description: "Turns the music loop mode on or off.",
+  description: "aktifin mode pengulangan lagu",
   permissions: "0x0000000000000800",
   options: [],
   voiceChannel: true,
@@ -25,19 +25,19 @@ module.exports = {
     try {
       const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
       const queue = client.player.getQueue(interaction.guild.id);
-      if (!queue || !queue.playing) return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true }).catch(e => { })
+      if (!queue || !queue.playing) return interaction.reply({ content: 'gada musiknya coy!!', ephemeral: true }).catch(e => { })
   
       let button = new ActionRowBuilder().addComponents(
         new ButtonBuilder()
-          .setLabel("Queue")
+          .setLabel("Antrian")
           .setStyle(ButtonStyle.Secondary)
           .setCustomId("queue"),
         new ButtonBuilder()
-          .setLabel("Current Song")
+          .setLabel("Lagu Sekarang")
           .setStyle(ButtonStyle.Secondary)
           .setCustomId("nowplaying"),
         new ButtonBuilder()
-          .setLabel("Stop Loop!")
+          .setLabel("Matikan Loop!")
           .setStyle(ButtonStyle.Danger)
           .setCustomId("close")
       )
@@ -45,11 +45,11 @@ module.exports = {
       const embed = new EmbedBuilder()
         .setColor('#fc4e03')
         .setAuthor({
-        name: 'Loop Your Melodies',
+        name: 'putar ulang lagumu',
         iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157318080670728283/7905-repeat.gif?ex=65182bf4&is=6516da74&hm=9ae58f40fcea5dc42a2a992bbd159d50116b3bafe5c5f7728e3a5276442efd2a&', 
-        url: 'https://discord.gg/FUEHs7RCqz'
+        url: 'https://discord.gg/cbxcCQdJsj'
     })
-        .setDescription('**Looping it! Let the music play on and on. **')
+        .setDescription('**puter ulang terus smpe budeg. sampe earwarm aku ga tanggung jawab!**')
      
       interaction?.reply({ embeds: [embed], components: [button], fetchReply: true }).then(async Message => {
 
@@ -60,27 +60,27 @@ module.exports = {
           if (button.user.id !== interaction.user.id) return
           const queue1 = client.player.getQueue(interaction.guild.id);
           if (!queue1 || !queue1.playing) {
-            await interaction?.editReply({ content: '⚠️ No music playing!!', ephemeral: true }).catch(e => { })
+            await interaction?.editReply({ content: 'gada musiknya coy!!', ephemeral: true }).catch(e => { })
             await button?.deferUpdate().catch(e => {})
           }
           switch (button.customId) {
             case 'queue':
               const success = queue.setRepeatMode(2);
-              interaction?.editReply({ content: `✅ Looping Queue!!` }).catch(e => { })
+              interaction?.editReply({ content: `putar ulang antrian!!` }).catch(e => { })
               await button?.deferUpdate().catch(e => {})
               break
             case 'nowplaying':
               const success2 = queue.setRepeatMode(1);
-              interaction?.editReply({ content: `✅ Looping activated!!` }).catch(e => { })
+              interaction?.editReply({ content: `mode looping aktif!!` }).catch(e => { })
               await button?.deferUpdate().catch(e => {})
               break
             case 'close':
               if (queue.repeatMode === 0) {
                 await button?.deferUpdate().catch(e => {})
-                return interaction?.editReply({ content: '⚠️ Looping already Off!!', ephemeral: true }).catch(e => { })
+                return interaction?.editReply({ content: 'mode looping dimatikan!!', ephemeral: true }).catch(e => { })
               }
               const success4 = queue.setRepeatMode(0);
-              interaction?.editReply({ content: '▶️ Looping off' }).catch(e => { })
+              interaction?.editReply({ content: 'mode looping mati' }).catch(e => { })
               await button?.deferUpdate().catch(e => {})
               break
           }
