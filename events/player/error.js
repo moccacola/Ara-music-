2db48bd78ca96743754de719885b414ac7123429
moccacola <158811185+moccacola@users.noreply.github.com>
2024-02-04 22:04:25 +0700

    error.js

--- events/player/error.js
+++ events/player/error.js
@@ -1,6 +1,6 @@
 module.exports = async (client, textChannel, e) => {
 if (textChannel){
-   return textChannel?.send(`**An error encountered:** ${e.toString().slice(0, 1974)}`)
+   return textChannel?.send(`**nahkan error:** ${e.toString().slice(0, 1974)}`)
 }
 }
 
