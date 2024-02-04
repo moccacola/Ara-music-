a2d5c6023ba27f5f9c9e68050b4cfd5a42a3077e
moccacola <158811185+moccacola@users.noreply.github.com>
2024-02-04 22:42:08 +0700

    owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -18,23 +18,23 @@ const db = require("../mongoDB");
 
 module.exports = {
   name: "owner",
-  description: "Get information about bot owner.",
+  description: "mau tau creatorku?",
   permissions: "0x0000000000000800",
   options: [],
 
   run: async (client, interaction) => {
     try {
-      const youtubeLink = 'https://discord.gg/FUEHs7RCqz';
-      const InstagramLink = 'https://www.instagram.com/rtxxgg/';
+      const youtubeLink = 'https://www.youtube.com/@lawrej';
+      const InstagramLink = 'https://www.instagram.com/princeprima1/';
       const { EmbedBuilder } = require('discord.js')
         const embed = new EmbedBuilder()
             .setColor('#da2a41')
             .setAuthor({
           name: 'Owner',
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157310253520662638/2443-iconperson.png?ex=651824aa&is=6516d32a&hm=0becc4a0fda01e5a02a63cf098db30c287e60a474f8d2da4ddeae7f47d98a5a3&',
-          url: 'https://discord.gg/FUEHs7RCqz'
+          url: 'https://discord.gg/cbxcCQdJsj'
         })
-            .setDescription(`__**About me**__:\n\n ▶️ Myself Shiva aka RTX. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n YouTube : ❤️ [RTX GAMING](${youtubeLink})\n Instagram : 💙 [rtxxgg](${InstagramLink})`)
+            .setDescription(`__**Tentangku**__:\n\n Creatorku si Lawe Rejas aka Prince Primary. Tinggal di Yogyakarta. Masih kuliah dan semoga ga sampai jadi donatur kampus di AMIKOM Yogyakarta!\n Kunjungi profilku <@454520127623397376> atau akses link dibawah\nYouTube : [Lawe Rejas](${youtubeLink})\n Instagram : [Prince Primary](${InstagramLink})`)
             .setTimestamp();
       interaction.reply({ embeds: [embed] }).catch(e => {});
 
