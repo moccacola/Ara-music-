2524bfc269606bdc300d21ba979705018a7856f9
moccacola <158811185+moccacola@users.noreply.github.com>
2024-02-04 22:22:46 +0700

    seek.js

--- commands/seek.js
+++ commands/seek.js
@@ -2,11 +2,11 @@ const { ApplicationCommandOptionType, EmbedBuilder } = require('discord.js');
 const db = require("../mongoDB");
 module.exports = {
   name: "seek",
-  description: "jump to the timestamp",
+  description: "loncat ke spesifik waktu",
   permissions: "0x0000000000000800",
   options: [{
     name: "time",
-    description: "enter timestamp",
+    description: "masukin waktunya",
     type: ApplicationCommandOptionType.String,
     required: true
   }],
@@ -15,13 +15,13 @@ module.exports = {
     try {
 
       const queue = client.player.getQueue(interaction.guild.id);
-      if (!queue || !queue.playing) return interaction.reply({ content: `⚠️ No music playing!!`, ephemeral: true }).catch(e => { })
+      if (!queue || !queue.playing) return interaction.reply({ content: `gada musiknya coy!!`, ephemeral: true }).catch(e => { })
 
       let position = getSeconds(interaction.options.getString("position"))
       if(isNaN(position)) return interaction.reply({ content: `usage : 2:40`, ephemeral: true }).catch(e => { })
 
       queue.seek(position)
-      interaction.reply({ content: `▶️ **Taking you on a time-travel journey to the specified timestamp.**`}).catch(e => { })
+      interaction.reply({ content: `**nyah, kupindahin ke waktu yg kamu mau.**`}).catch(e => { })
 
     } catch (e) {
       console.error(e);
