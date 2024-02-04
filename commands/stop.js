cad80a046a112ef3aef4a06ed2ab16475964b49e
moccacola <158811185+moccacola@users.noreply.github.com>
2024-02-04 22:15:50 +0700

    stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -18,7 +18,7 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: "stop",
-  description: "Stops the music.",
+  description: "matiin musik",
   permissions: "0x0000000000000800",
   options: [],
   voiceChannel: true,
@@ -26,7 +26,7 @@ module.exports = {
     try {
       const queue = client.player.getQueue(interaction.guild.id);
       if (!queue || !queue.playing) {
-        return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true });
+        return interaction.reply({ content: 'gada musiknya coy!!', ephemeral: true });
       }
 
       queue.stop(interaction.guild.id);
@@ -36,9 +36,9 @@ module.exports = {
         .setAuthor({
           name: 'Music Stopped',
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157305318255116400/pngtree-vector-stop-icon-png-image_4233262.jpg?ex=65182011&is=6516ce91&hm=d5a8ca6010716bae836b025f8d36557a95f14c13a705f65eb09a54161649c795&',
-          url: 'https://discord.gg/FUEHs7RCqz'
+          url: 'https://discord.gg/cbxcCQdJsj'
         })
-        .setDescription('**The journey stops, but the rhythm lives on.**')
+        .setDescription('**cape juga ngebacot, aku rehat dulu lah.**')
         
 
       return interaction.reply({ embeds: [embed] });
