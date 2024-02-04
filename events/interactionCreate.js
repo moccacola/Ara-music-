2546f4b6238d29eff59ac764878ca7e4d008001c
moccacola <158811185+moccacola@users.noreply.github.com>
2024-02-04 22:01:29 +0700

    interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -21,7 +21,7 @@ module.exports = async (client, interaction) => {
 
 try {
 if (!interaction?.guild){
-return interaction?.reply({ content: "Rate Limited.", ephemeral: true })
+return interaction?.reply({ content: "Rate terbatas", ephemeral: true })
 } else {
 
     function cmd_loader() {
@@ -55,7 +55,7 @@ return interaction?.reply({ content: "Rate Limited.", ephemeral: true })
 
             if (!channel_filter?.length > 0 && !interaction?.member?.permissions?.has("0x0000000000000020")) {
             channel_filter = data?.channels?.map(x => `<#${x.channel}>`).join(", ")
-            return interaction?.reply({ content: '🔴 Rate Limited'.replace("{channel_filter}", channel_filter), ephemeral: true }).catch(e => { })
+            return interaction?.reply({ content: 'Rate terbatas'.replace("{channel_filter}", channel_filter), ephemeral: true }).catch(e => { })
             }
         }
             }
@@ -82,21 +82,21 @@ return interaction?.reply({ content: "Rate Limited.", ephemeral: true })
             }
             }
             if (props && props.voiceChannel) {
-            if (!interaction?.member?.voice?.channelId) return interaction?.reply({ content: `🔴 Join Voice channel First!!`, ephemeral: true }).catch(e => { })
+            if (!interaction?.member?.voice?.channelId) return interaction?.reply({ content: `lah u ga masuk voice, ogah!!`, ephemeral: true }).catch(e => { })
             const guild_me = interaction?.guild?.members?.cache?.get(client?.user?.id);
             if (guild_me?.voice?.channelId) {
             if (guild_me?.voice?.channelId !== interaction?.member?.voice?.channelId) {
-            return interaction?.reply({ content: `🔴 Must be in same VC!!`, ephemeral: true }).catch(e => { })
+            return interaction?.reply({ content: `masuk voice dlu kocak!!`, ephemeral: true }).catch(e => { })
             }
             }
             }
             return props.run(client, interaction);
             
             } else {
-            return interaction?.reply({ content: `▶️ Missing Permissions: **${props?.permissions?.replace("0x0000000000000020", "MANAGE GUILD")?.replace("0x0000000000000800", "SEND MESSAGES") || "SEND MESSAGES"}**`, ephemeral: true });
+            return interaction?.reply({ content: `▶️ Missing Permissions: **${props?.permissions?.replace("0x0000000000000020", "MANAGE GUILD")?.replace("0x0000000000000800", "SEND MESSAGES") || "Kirim Pesan"}**`, ephemeral: true });
             }
             } catch (e) {
-            return interaction?.reply({ content: `❌ Error...\n\n\`\`\`${e?.message}\`\`\``, ephemeral: true });
+            return interaction?.reply({ content: `nahkan Error...\n\n\`\`\`${e?.message}\`\`\``, ephemeral: true });
             }
             }
             });
